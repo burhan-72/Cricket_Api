f5eb9a66e5f0884284b8d384e2ee0cf4a1ede402
Kunal Bansal <kunalbansal@Kunals-MacBook-Air.local>
2021-10-19 17:50:28 +0530

    Added % days matches

--- index.js
+++ index.js
@@ -2,46 +2,58 @@ const express = require('express');
 const request = require('request');
 const PORT = 8000;
 const app = express();
-
 require('dotenv').config();
 
+app.set('view engine', 'ejs');
+app.set('views', './views');
 
+function pad2(n) {
+  return (n < 10 ? '0' : '') + n;
+}
 
-const options = {
-  method: 'GET',
-  url: 'https://cricket-live-data.p.rapidapi.com/fixtures-by-date/2021-10-19',
-  headers: {
-    'x-rapidapi-host': 'cricket-live-data.p.rapidapi.com',
-    'x-rapidapi-key': process.env.API_KEY,
-    useQueryString: true
-  }
+let obj = {
+  "results" : []
 };
+var date = new Date();
+const date1 = new Date();
+var month = pad2(date.getMonth()+1);//months (0-11)
+var day = pad2(date.getDate());//day (1-31)
+var year= date.getFullYear();
+var formattedDate =  year+"-"+month+"-"+day;
+for (let i = 0; i < 5; i++) {
+  console.log(formattedDate);
+  const options = {
+    method: 'GET',
+    url: `https://cricket-live-data.p.rapidapi.com/fixtures-by-date/${formattedDate}`,
+    headers: {
+      'x-rapidapi-host': 'cricket-live-data.p.rapidapi.com',
+      'x-rapidapi-key': process.env.API_KEY,
+      useQueryString: true 
+    } 
+  };
 
-app.get('/',function(req,res){
   request(options, function (error, response, body) {
     if (error) throw new Error(error);
-  
-    res.end(body);
-
-    const obj=body;
-    // console.log();
-
-    console.log(obj.results);
-    // console.log(body.results);
-    // for(matches of body.results){
-    //   console.log(matches.match_title ,'\n');
-    // }
-    
+    let s = JSON.parse(body);
+    console.log(s); 
+    for(mat of s.results){
+      obj.results.push(mat);
+    }
   });
+  date = new Date(date.getTime() + (24 * 60 * 60 * 1000));
+  var month = pad2(date.getMonth()+1);//months (0-11)
+  var day = pad2(date.getDate());//day (1-31)
+  var year= date.getFullYear();
+  formattedDate =  year+"-"+month+"-"+day;
+}
+
+app.get('/',function(req,res){
+  return res.render('card', {
+    title: 'Home Page',
+    matches: obj,
+    currDate: date1
+  })
 });
-
-// request(options, function (error, response, body) {
-// 	if (error) throw new Error(error);
-
-// 	// console.log(body,null,2);
-//   console.log(JSON.stringify(body,null,2));
-  
-// });
 
 app.listen(PORT,function(err){
   if(err){

8268e23a02caf8d240c2f8c7cb8a8f34f65628d6
Kunal Bansal <kunalbansal@Kunals-MacBook-Air.local>
2021-11-01 14:36:54 +0530

    Added Selected Player in Array

--- assets/js/contest-card.js
+++ assets/js/contest-card.js
@@ -1,11 +1,36 @@
 const createBtn=document.getElementById('create-team-button');
 createBtn.addEventListener('click',function(){
-    console.log('Button Clicked');
     const div=document.getElementById('select-players');
     if(div.style.display==="none"){
         div.style.display="block";
     }else{
         div.style.display="none";
     }
-
 });
+
+const addPlayer = document.querySelectorAll('.player-add-icon');
+var addedPlayers = [];
+for(let i = 0; i < addPlayer.length; i++){
+    let player = addPlayer[i];
+    player.addEventListener('click', function(){
+        const playerName = player.getAttribute('data-player-info');
+        const isPresent = addedPlayers.indexOf(playerName);
+        console.log(isPresent);
+        if(isPresent > -1){
+            const index = addedPlayers.indexOf(playerName);
+            if (index > -1) {
+                addedPlayers.splice(index, 1);
+            }
+            player.parentElement.parentElement.style.backgroundColor = "black";
+        }
+        else{
+            if(addedPlayers.length == 11){
+                alert('Cannot Add more than 11 players');
+                return;
+            }
+            addedPlayers.push(playerName);
+            player.parentElement.parentElement.style.backgroundColor = "#00b137";
+        }
+        console.log(addedPlayers);
+    });
+}
